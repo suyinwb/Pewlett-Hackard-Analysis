db32ec7016287e7dd9d9f9cbb93921ea22491343
suyinwb <wong.suyin@gmail.com>
2021-12-06 01:37:12 -0500

    alias

--- queries.sql
+++ queries.sql
@@ -65,8 +65,16 @@ ON departments.dept_no = dept_manager.dept_no;
 -- Joining retirement_info and dept_emp tables
 SELECT retirement_info.emp_no,
     retirement_info.first_name,
-retirement_info.last_name,
+    retirement_info.last_name,
     dept_emp.to_date
 FROM retirement_info
 LEFT JOIN dept_emp
 ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
