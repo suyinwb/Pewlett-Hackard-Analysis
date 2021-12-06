adea37770ee9e9df0f79414fa14a224840109088
suyinwb <wong.suyin@gmail.com>
2021-12-06 01:41:01 -0500

    current_emp

--- queries.sql
+++ queries.sql
@@ -78,3 +78,21 @@ SELECT ri.emp_no,
 FROM retirement_info as ri
 LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no;
+
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+ FROM departments as d
+ INNER JOIN dept_manager as dm
+ ON d.dept_no = dm.dept_no;
+
+ SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
