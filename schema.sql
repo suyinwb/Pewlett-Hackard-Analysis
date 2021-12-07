c44ff86d23ce77035e6a52bb17f157df08a0aff5
suyinwb <wong.suyin@gmail.com>
2021-12-06 20:47:24 -0500

    schema

--- schema.sql
+++ schema.sql
@@ -23,7 +23,6 @@ CREATE TABLE dept_manager (
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE dept_emp (
