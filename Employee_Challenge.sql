1b7ec3c7d2f4c8a95c7f346b0c7e3653da65138c
suyinwb <wong.suyin@gmail.com>
2021-12-12 15:01:13 -0500

    dept titles retiring

--- Employee_Challenge.sql
+++ Employee_Challenge.sql
@@ -17,7 +17,7 @@ ORDER BY emp_no ASC , from_date DESC;
 
 --Retiring Titles
 SELECT title, count(emp_no) AS "Total Retiring"
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count(emp_no) DESC;
@@ -33,3 +33,16 @@ JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no ASC;
+
+
+--APPENDIX
+-- Find out which dept and job title has highest retiring staff
+SELECT ut.title, d.dept_name, count(ut.emp_no) AS "Total Retiring"
+INTO dept_titles_retiring
+FROM unique_titles AS ut
+JOIN dept_emp AS de
+ON (ut.emp_no = de.emp_no)
+JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+GROUP BY d.dept_name, ut.title
+ORDER BY count(ut.emp_no) DESC;
